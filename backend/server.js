7efebedc673ebf77fe751be8cf091edc20a73c39
Jagadish U Aradhya <153201075+jagadishuaradhya@users.noreply.github.com>
2025-11-01 16:08:06 +0530

    Commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const routes = require("./routes/ToDoroutes");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected...."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
